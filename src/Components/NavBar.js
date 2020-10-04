30024959363318bf231556e34b08af0ae6cf3dd7
Mustafa Muhammad <mustafamuhammad0508@gmail.com>
2020-10-03 20:10:11 -0600

    Found name for app

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 export const NavBar = () => {
   return (
     <div>
-      <navbar>Temp Title</navbar>
+      <navbar>Fortuna</navbar>
     </div>
   );
 };
--- src/Components/Title.js
+++ src/Components/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Title = () => {
   return (
     <div>
-      <h2>Welcome to (Temp Title)!</h2>
+      <h2>Welcome to Fortuna!</h2>
     </div>
   );
 };
